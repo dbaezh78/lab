cc4dd84816c81e1d8c3cdedcfcb95f86457f5e3c
dbaezh78 <108948897+dbaezh78@users.noreply.github.com>
2026-07-30 13:19:05 -0400

    Corregir posicionamiento del selector de color abriendolo sobre el boton en lugar de en la esquina

--- resucito/sw.js
+++ resucito/sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker para el cancionero Resucito
-const CACHE_NAME = 'resucito-cache-v14';
+const CACHE_NAME = 'resucito-cache-v15';
 const STATIC_ASSETS = [
   './',
   'index.html',
